3163bef3c7fa3ce65e85d0097d668719a813cfd1
colto <colton11gerth@gmail.com>
2021-08-31 10:39:51 -0500

    cleaned up web ui

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__, template_folder="templates")
+
+#Routing for html template files
+@app.route('/')
+@app.route('/index')
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/homecontent', methods=['GET'])
+def homecontent():
+    return render_template('homecontent.html')
+
+@app.route('/about', methods=['GET'])
+def test():
+    return render_template('about.html')
+
+@app.route('/advanced', methods=['GET'])
+def advanced():
+    return render_template('advanced.html')
+
+@app.route('/references', methods=['GET'])
+def references():
+    return render_template('references.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ static/homerow.js
@@ -0,0 +1,84 @@
+// Tab variables for hero banner
+home = document.getElementById('home');
+about = document.getElementById('about');
+advanced = document.getElementById('advanced');
+references = document.getElementById('references');
+contact = document.getElementById('contact');
+files = document.getElementById('files');
+
+
+pagecontainer = document.getElementById('pagecontent');
+pagecontainer.onload = function(){
+    pagecontainer.style.height = pagecontainer.contentWindow.document.body.scrollHeight + 'px';
+}
+
+// Event listeners for tab variables
+
+home.addEventListener("click", (e) => {
+    if(home.classList.contains('herotab')==false || document.getElementById('pagecontent').src != '/homecontent'){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        home.classList.add('herotab');
+        home.classList.add('has-test-white');
+        temp = '';
+        document.getElementById('pagecontent').src = "/homecontent";
+    }
+
+});
+about.addEventListener("click", (e) => {
+    if(about.classList.contains('herotab')==false || document.getElementById('pagecontent').src != '/about'){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        about.classList.add('herotab');
+        about.classList.add('has-test-white');
+        temp = '';
+        document.getElementById('pagecontent').src = "/about";
+
+    }
+});
+advanced.addEventListener("click", (e) => {
+    if(advanced.classList.contains('herotab')==false || document.getElementById('pagecontent').src != '/advanced'){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        advanced.classList.add('herotab');
+        advanced.classList.add('has-test-white');
+        temp = '';
+        document.getElementById('pagecontent').src = "/advanced";
+    }
+});
+references.addEventListener("click", (e) => {
+    if(references.classList.contains('herotab')==false || document.getElementById('pagecontent').src != '/references'){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        references.classList.add('herotab');
+        references.classList.add('has-test-white');
+        temp = '';
+        document.getElementById('pagecontent').src = "/references";
+    }
+});
+contact.addEventListener("click", (e) => {
+    if(contact.classList.contains('herotab')==false || document.getElementById('pagecontent').src != '/contact'){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        contact.classList.add('herotab');
+        contact.classList.add('has-test-white');
+        temp = '';
+        document.getElementById('pagecontent').src = "/contact";
+        
+    }
+});
+files.addEventListener("click", (e) => {
+    if(files.classList.contains('herotab')==false){
+        temp = document.getElementsByClassName('herotab');
+        temp[0].classList.remove('has-text-white');
+        temp[0].classList.remove('herotab');
+        files.classList.add('herotab');
+        files.classList.add('has-test-white');
+        temp = '';
+    }
+});
