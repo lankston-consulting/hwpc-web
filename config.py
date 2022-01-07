90a3e8a94252c948dcb5a4b7ed78500e37d86fea
colto <colton11gerth@gmail.com>
2022-01-07 15:01:50 -0600

    .env compatibility

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+import os
+from dotenv import load_dotenv
 from utils import gcs_helper
 """Credentials for RPMS App."""
 
-
-# The private key associated with your service account in JSON format.
-GCP_PRIVATE_KEY_FILE = 'hwpc-sa.json'
+load_dotenv()
 
 ###############################################################################
 #                               Initialization.                               #
 ###############################################################################
-# gch = gcs_helper.GcsHelper(use_service_account={'keyfile': GCP_PRIVATE_KEY_FILE})
-gch = gcs_helper.GcsHelper()
+json_keyfile = os.getenv('SERVICE_ACCOUNT_FILE')
+if os.path.exists(json_keyfile):
+    gch = gcs_helper.GcsHelper(use_service_account={'keyfile': json_keyfile})
+else:
+    gch = gcs_helper.GcsHelper()
