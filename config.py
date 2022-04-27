e08a90dd38fe5aa1d48f612198bfd8564bb2f00e
Colton <colton@lankstonconsulting.com>
2022-04-27 17:55:12 -0500

    update attempt number 2

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
+from lcutils import gcs
 from dotenv import load_dotenv
-from utils import gcs_helper
+#from utils import gcs_helper
 """Credentials for RPMS App."""
 
 load_dotenv()
@@ -11,6 +12,6 @@ load_dotenv()
 ###############################################################################
 json_keyfile = os.getenv('SERVICE_ACCOUNT_FILE')
 if os.path.exists(json_keyfile):
-    gch = gcs_helper.GcsHelper(use_service_account={'keyfile': json_keyfile})
+    gch = gcs.GcsTools(use_service_account={'keyfile': json_keyfile})
 else:
-    gch = gcs_helper.GcsHelper()
+    gch = gcs.GcsTools()
