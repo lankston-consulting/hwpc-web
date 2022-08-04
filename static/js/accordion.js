2505bc6d0499f1fbd6f766191045e955be2fdf32
Claire Spain <clairespain@outlook.com>
2022-08-04 12:33:38 -0500

    Updated accordion next prev btns

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -16,7 +16,7 @@ if($.inArray(name,headers) > -1) {
   
   //slideUp all elements (except target) at current depth or greater
   var depth = $(subItem).parents().length;
-  var allAtDepth = $(".accordion p, .accordion div").filter(function() {
+  var allAtDepth = $(".accordion div").filter(function() {
     if($(this).parents().length >= depth && this !== subItem.get(0)) {
       return true; 
     }
@@ -40,24 +40,37 @@ $(".accordion").keydown(function (event) {
     if (event.which === 32 || event.which === 13) { //32 is Space and 13 is Enter
         toggleAccordion(event);
     }
-})
+});
 
 $(".nextbtn").click(function (e) {
-  var valid = true;
-    for(i=0;i<$(e.target).siblings().length;i++){
-      if($(e.target).siblings()[i].localName=="input"){
-        if($(e.target).siblings()[i].validity.valid == false){
-          valid = false
+    var valid = true;
+    console.log($(e.target).siblings())
+    for (i = 0; i < $(e.target).siblings().length; i++) {
+        if ($(e.target).siblings()[i].localName == "input") {
+            if ($(e.target).siblings()[i].validity.valid == false) {
+                valid = false
+            }
         }
-      }
     }
-    if(valid==true){
-      toggleAccordion($(e.target).parent()[0].previousElementSibling)
-      toggleAccordion($(e.target).parent()[0].nextElementSibling)
+    if (valid == true) {
+        toggleAccordion($(e.target).parent()[0].previousElementSibling)
+        toggleAccordion($(e.target).parent()[0].nextElementSibling)
     }
-})
+});
 
 $(".backbtn").click(function (e) {
     toggleAccordion($(e.target).parent()[0].previousElementSibling)
     toggleAccordion($(e.target).parent()[0].previousElementSibling.previousElementSibling.previousElementSibling)
-})
+});
+
+$(document).ready(function () {
+    $('input[type="radio"]').click(function() {
+        if($(this).attr('id') == 'customRatio') {
+             $('#custom-file-upload').show();           
+        }
+ 
+        else {
+             $('#custom-file-upload').hide();   
+        }
+    });
+});
