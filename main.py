e7584687d08f46f41deab9d8b49d1445ef08f7d3
cg117634 <colton11gerth@gmail.com>
2022-09-08 16:11:46 -0500

    clean up of upload testing code now that lcutils is updated

--- main.py
+++ main.py
@@ -54,9 +54,6 @@ def files():
 def upload():
     # All inputs from the UI are pulled here through with jquery Ajax
     yearly_harvest_input = request.files['yearlyharvestinput']
-    # yearly_harvest_input.seek(0, os.SEEK_END)
-    # print(yearly_harvest_input.tell())
-    print(yearly_harvest_input.filename)
     if(yearly_harvest_input.filename != ''):
         yearly_harvest_input = pd.read_csv(yearly_harvest_input)
         
@@ -66,15 +63,18 @@ def upload():
                                                                 var_name="Year",
                                                                 value_name="ccf")
             yearly_harvest_input= yearly_harvest_input[yearly_harvest_input['ccf'] != 0]
+            yearly_harvest_input = yearly_harvest_input.to_csv(index=False)
             print(yearly_harvest_input)
-            with tempfile.NamedTemporaryFile() as fp:
-                yearly_harvest_input.to_csv(fp.name, index=False)
-                yearly_harvest_input = fp
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     yearly_harvest_input.to_csv(fp.name, index=False)
+            #     yearly_harvest_input = fp
         else: 
+            yearly_harvest_input = yearly_harvest_input.to_csv(index=False)
+            print(yearly_harvest_input)
             # yearly_harvest_input.to_csv("/tmp/yearly_harvest_data.csv", index=False)
-            with tempfile.NamedTemporaryFile() as fp:
-                yearly_harvest_input.to_csv(fp.name, index=False)
-                yearly_harvest_input = fp
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     yearly_harvest_input.to_csv(fp.name, index=False)
+            #     yearly_harvest_input = fp
         
     harvest_data_type = request.form['harvestdatatype']
     timber_product_ratios = request.files['yearlytimberproductratios']
@@ -86,13 +86,15 @@ def upload():
             timber_product_ratios = timber_product_ratios.melt(id_vars="TimberProductID",
                                                                      var_name="Year",
                                                                      value_name="Ratio")
-            with tempfile.NamedTemporaryFile() as fp:
-                timber_product_ratios.to_csv(fp.name, index=False)
-                timber_product_ratios = fp
+            timber_product_ratios = timber_product_ratios.to_csv(index=False)
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     timber_product_ratios.to_csv(fp.name, index=False)
+            #     timber_product_ratios = fp
         else:
-            with tempfile.NamedTemporaryFile() as fp:
-                timber_product_ratios.to_csv(fp.name, index=False)
-                timber_product_ratios = fp
+            timber_product_ratios = timber_product_ratios.to_csv(index=False)
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     timber_product_ratios.to_csv(fp.name, index=False)
+            #     timber_product_ratios = fp
     region_selection = request.form['regionselection']
     if(region_selection == "Custom"):
         custom_region_file = request.files['customregion']
@@ -102,14 +104,15 @@ def upload():
                 custom_region_file = custom_region_file.melt(id_vars="PrimaryProductID",
                                                                         var_name="Year",
                                                                         value_name="Ratio")
-                
-                with tempfile.NamedTemporaryFile() as fp:
-                    custom_region_file.to_csv(fp.name, index=False)
-                    custom_region_file = fp
+                custom_region_file = custom_region_file.to_csv(index=False)
+                # with tempfile.NamedTemporaryFile() as fp:
+                #     custom_region_file.to_csv(fp.name, index=False)
+                #     custom_region_file = fp
             else:
-                with tempfile.NamedTemporaryFile() as fp:
-                    custom_region_file.to_csv(fp.name, index=False)
-                    custom_region_file = fp
+                custom_region_file = custom_region_file.to_csv(index=False)
+                # with tempfile.NamedTemporaryFile() as fp:
+                #     custom_region_file.to_csv(fp.name, index=False)
+                #     custom_region_file = fp
     else:
         custom_region_file = ""
     end_use_product_ratios = request.files['EndUseRatiosFilename']
@@ -119,16 +122,17 @@ def upload():
             end_use_product_ratios = end_use_product_ratios.melt(id_vars="EndUseID",
                                                                     var_name="Year",
                                                                     value_name="Ratio")
-            with tempfile.NamedTemporaryFile() as fp:
-                end_use_product_ratios.to_csv(fp.name, index=False)
-                end_use_product_ratios = fp
+            end_use_product_ratios = end_use_product_ratios.to_csv(index=False)
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     end_use_product_ratios.to_csv(fp.name, index=False)
+            #     end_use_product_ratios = fp
         else:
-            with tempfile.NamedTemporaryFile() as fp:
-                custom_region_file.to_csv(fp.name, index=False)
-                custom_region_file = fp
-    # end_use_half_lives = request.files['EndUseHalfLivesFilename']
+            end_use_product_ratios = end_use_product_ratios.to_csv(index=False)
+            # with tempfile.NamedTemporaryFile() as fp:
+            #     custom_region_file.to_csv(fp.name, index=False)
+            #     custom_region_file = fp
     dispositions = request.files['DispositionsFilename']
-    # disposition_half_lives = request.files['DispositionHalfLivesFilename']
+    disposition_half_lives = request.files['DispositionHalfLivesFilename']
     distribution_data = request.files['DistributionDataFilename']
     burned_ratios = request.files['BurnedRatiosFilename']
     mbf_to_ccf = request.files['MbfToCcfFilename']
@@ -148,6 +152,7 @@ def upload():
             "primary_product_ratios.csv":custom_region_file,
             "end_use_product_ratios.csv":end_use_product_ratios,
             "dispositions.csv":dispositions,
+            "disposition_half_lives.csv":disposition_half_lives,
             "distribution_data.csv":distribution_data,
             "burned_ratios.csv":burned_ratios,
             "mbf_to_ccf.csv":mbf_to_ccf,
@@ -162,10 +167,11 @@ def upload():
     data_type = type(harvest_data_type)
     new_id = str(uuid.uuid4())
     # print(new_id)
-    upload_input_group("hwpcarbon-data", user_data_folder + new_id + '/', data , data_type)
+    gch.upload_input_group("hwpcarbon-data", user_data_folder + new_id + '/', data , data_type)
     #return "This is a test to view the submitted data"   
     # return render_template('pages/submit.html', file_path=user_data_folder + new_id + '/', run_name=run_name, run_path = 'https://hwpc-calculator-3d43jw4gpa-uw.a.run.app' + '/?p=' + user_data_folder + new_id + '&q=' + run_name)
     return render_template('pages/submit.html')
+
 @app.route('/submit')
 def submit():
     return render_template("pages/submit.html")
@@ -208,57 +214,6 @@ def output():
 
     return render_template("pages/output.html",data_json=data_json)
 
-#FOR TESTING PURPOSES DELETE WHEN DONE
-def upload_input_group(bucket_name, source_file_name, data, data_type):
-        """[summary]
-        This helper function that uploads a data cluster of user inputs to a unique folder, likely named with a user generated hash id.
-
-        This function is specially modified and used in hwpc-web
-
-        Args:
-            bucket_name ([type]): The name of the destination bucket you are uploading to.
-            source_file_name ([type]): The name of the destination folder for the user date, this consists of "hwpc-user-inputs" and the user's generated id.
-            data ([type]): The data to be delivered to the destination folder.
-            data_type ([type]): A logic check for harvested_wood_products.
-        """
-        
-        data_json = {}
-        # Code parses through data pulled from web
-        for key,value in data.items():
-            # If the input type is not a file type, it will write a text file with information and push it up to cloud storage
-            print("this"+str(type(value)))
-            print(value)
-            if str(type(value)) != "<class 'tempfile._TemporaryFileWrapper'>":
-                print("in here")
-                # If the input is not empty, it will make the file and upload. If it is empty, it will be skipped and save memory.
-                if (value != ""):
-                    path = source_file_name+key
-                    temp = value.encode()
-                    temp_file = tempfile.TemporaryFile()
-                    temp_file.write(temp)
-                    temp_file.seek(0)
-                    data_json[key]=path
-                    gch.upload_temp(bucket_name, temp_file, path)
-                    temp_file.close()
-            # If it is a file type, it will directly upload the file instead
-            else:
-                # If the input is not empty, it will make the file and upload. If it is empty, it will be skipped and save memory.
-                print( type(value))
-            # if  type(value) == "<class 'tempfile._TemporaryFileWrapper'>":
-                path = source_file_name+key
-                data_json[key]=path
-                    # gch.upload_temp(bucket_name, value, path)
-            
-        # The json of all the file paths is converted into a string then to bytes to be uploaded as a temp file for use in the worker.
-        data_json = json.dumps(data_json)
-        data_json = data_json.encode()
-        user_file = tempfile.TemporaryFile()
-        user_file.write(data_json)
-        user_file.seek(0)
-        print(bre)
-        gch.upload_temp(bucket_name, user_file, source_file_name + "user_input.json")
-        user_file.close()
-        return
 # @app.route('/download', methods=['GET','POST'])
 # def download():
     
