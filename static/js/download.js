fd29a5b9056333d68ee441f7ffbad760829ceb80
Claire Spain <clairespain@outlook.com>
2022-09-22 13:09:47 -0500

    Quick Download fix for Demo

--- static/js/download.js
+++ static/js/download.js
@@ -32,51 +32,51 @@ d3.select("#download")
         // tempLink.click();
       }
 
-        // zip all plotly downloadImages as a zip file and download
-        var zip = new JSZip();
-        var img = zip.folder("images");
-        img.file("plot.png", imgData, { base64: true });
-        zip.generateAsync({ type: "blob" })
-          .then(function (content) {
-            // see FileSaver.js
-            saveAs(content, "example.zip");
-          });
+      // zip all plotly downloadImages as a zip file and download
+      // var zip = new JSZip();
+      // var img = zip.folder("images");
+      // img.file("plot.png", imgData, { base64: true });
+      // zip.generateAsync({ type: "blob" })
+      //   .then(function (content) {
+      //     // see FileSaver.js
+      //     saveAs(content, "example.zip");
+      //   });
      
 
-        //     // saveSvgAsPng(document.getElementById(tempChk.value+"1"), tempChk.value +".png", {backgroundColor: "#FFFFFF", width: 1300, height: 700});
-        //     var serializer = new XMLSerializer(),
-        //     svgString = serializer.serializeToString(document.getElementById(tempChk.value+ "1"));
+      //     // saveSvgAsPng(document.getElementById(tempChk.value+"1"), tempChk.value +".png", {backgroundColor: "#FFFFFF", width: 1300, height: 700});
+      //     var serializer = new XMLSerializer(),
+      //     svgString = serializer.serializeToString(document.getElementById(tempChk.value+ "1"));
 
-        //     var svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
-        //     var url = URL.createObjectURL(svg);
-        //     var img = new Image();
-        //     img.onload = function() {
-        //       // draw the svg to a canvas
+      //     var svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
+      //     var url = URL.createObjectURL(svg);
+      //     var img = new Image();
+      //     img.onload = function() {
+      //       // draw the svg to a canvas
   
-        //       ctx.drawImage(img, 0, 0);
-        //       URL.revokeObjectURL(url);
-        //       canvas.toBlob(function (blob) { // <-- convert the canvas to a png (in a blob)
+      //       ctx.drawImage(img, 0, 0);
+      //       URL.revokeObjectURL(url);
+      //       canvas.toBlob(function (blob) { // <-- convert the canvas to a png (in a blob)
 
-        //         var zip = new JSZip();
-        //         // zip.file("abc.csv", CSV); 
-        //         zip.file(tempChk.value + ".png", blob); // <-- JSZip v3 accepts blob
+      //         var zip = new JSZip();
+      //         // zip.file("abc.csv", CSV); 
+      //         zip.file(tempChk.value + ".png", blob); // <-- JSZip v3 accepts blob
 
-        //         content = zip.generateAsync({type:"blob"}).then(function (blob) {
-        //           saveAs(blob, "result.zip"); // <-- trigger the download
-        //         }, function (e) {
-        //           console.error(e)
-        //         });
-        //       }, "image/png");
-        //     };
-        //     img.src = url; 
-        //   } else {
-        //     // return document.getElementById("result").innerHTML = "Please select at least one file to download.";
-        //     console.log("No files to download.");
-      }
-    })
+      //         content = zip.generateAsync({type:"blob"}).then(function (blob) {
+      //           saveAs(blob, "result.zip"); // <-- trigger the download
+      //         }, function (e) {
+      //           console.error(e)
+      //         });
+      //       }, "image/png");
+      //     };
+      //     img.src = url; 
+      //   } else {
+      //     // return document.getElementById("result").innerHTML = "Please select at least one file to download.";
+      //     console.log("No files to download.");
+    
+    });
 
   
+
   });
-
 
 
