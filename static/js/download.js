9375f65b648767a0f7f91e08c27736d8be724573
Claire Spain <clairespain@outlook.com>
2022-09-23 13:14:40 -0500

    Download Modal Zip Benchmark

--- static/js/download.js
+++ static/js/download.js
@@ -11,94 +11,125 @@
      
 var img_png = d3.select('#png-export');
 
-d3.select("#download")
-  .on('click', function () {
-    // console.log(document.getElementById("end_use"));
+function export_tables() {
+  //export plotly tables as pngs
+  var tables = document.getElementsByClassName('table');
+  for (var i = 0; i < tables.length; i++) {
+    Plotly.downloadImage(tables[i], { format: 'png' });
 
-    //   saveSvgAsPng(document.getElementById("end_use"), "plot.png", {backgroundColor: "#FFFFFF", width: 1300, height: 700});
-    image_array = [];
-    $(".dl-files").children().each(function () {
-      tempChk = $(this).children()[0];
+  }
+  //save pngs as pdfs
 
-      if (tempChk.checked) {
-        // console.log(tempChk.value);
-        Plotly.toImage(document.getElementsByClassName(tempChk.value)[0], { format: 'png', width: 1300, height: 700 }).then(function (dataUrl) {
+  //export pdfs as zip
+}
+
+
+function export_plots() {
+  var image_array = [];
+  var zip = new JSZip();
+  $(".dl-files").children().each(function () {
+    tempChk = $(this).children()[0];
+    if (tempChk.checked) {
+      // console.log(tempChk.value);
+      png_options = { format: 'png', width: 1300, height: 700 };
+       //push plotly to image_array
       
-          $("#hidden-graphs").append("<img src='" + dataUrl + "' class='hidden-graph'>");
-          img_png.attr("src", dataUrl);
-          image_array[tempChk.value] = img_png;
-          console.log(image_array[tempChk.value]);
-
-      
-      })
-        
-        
-
-        //export all csv files
-        // var csv = document.getElementsByClassName("csv")[0];
-        // var csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-        // var csvURL = window.URL.createObjectURL(csvData);
-        // var tempLink = document.createElement('a');
-        // tempLink.href = csvURL;
-        // tempLink.setAttribute('download', 'data.csv');
-        // tempLink.click();
-      }
-
-      // zip all plotly downloadImages as a zip file and download
-   
+      // 
+       gen = generate_image(document.getElementsByClassName(tempChk.value)[0], png_options,tempChk.value.slice(0, -7) + ".png",zip)
+      console.log(gen)
      
-
-      //     // saveSvgAsPng(document.getElementById(tempChk.value+"1"), tempChk.value +".png", {backgroundColor: "#FFFFFF", width: 1300, height: 700});
-      //     var serializer = new XMLSerializer(),
-      //     svgString = serializer.serializeToString(document.getElementById(tempChk.value+ "1"));
-
-      //     var svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
-      //     var url = URL.createObjectURL(svg);
-      //     var img = new Image();
-      //     img.onload = function() {
-      //       // draw the svg to a canvas
-  
-      //       ctx.drawImage(img, 0, 0);
-      //       URL.revokeObjectURL(url);
-      //       canvas.toBlob(function (blob) { // <-- convert the canvas to a png (in a blob)
-
-      //         var zip = new JSZip();
-      //         // zip.file("abc.csv", CSV); 
-      //         zip.file(tempChk.value + ".png", blob); // <-- JSZip v3 accepts blob
-
-      //         content = zip.generateAsync({type:"blob"}).then(function (blob) {
-      //           saveAs(blob, "result.zip"); // <-- trigger the download
-      //         }, function (e) {
-      //           console.error(e)
-      //         });
-      //       }, "image/png");
-      //     };
-      //     img.src = url; 
-      //   } else {
-      //     // return document.getElementById("result").innerHTML = "Please select at least one file to download.";
-      //     console.log("No files to download.");
-    
-    });
-    console.log(image_array);
-     var zip = new JSZip();
-    for (var key in image_array){
-      console.log(key.slice(0,-7))
-      console.log(image_array[key])
-      zip.file(key.slice(0,-7)+".png",image_array[key])
-    }
-    // for (i = 0; i < image_array.length; i++) {
-    //   
       
-    // }
-   
-    // img.file("plot.png", imgData, { base64: true });
-    zip.generateAsync({ type: "blob" })
-      .then(function (content) {
-        // see FileSaver.js
-        saveAs(content, "example.zip");
+      console.log(image_array)
+    }
+  })
+  console.log(zip)
+  zip.generateAsync({ type: "blob" }).then(function callback(content) {
+    // see FileSaver.js
+    console.log(content)
+    saveAs(content, "example.zip");});
+}
+
+async function generate_image(div, options,file_name,zip) {
+  url = await Plotly.toImage(div, options);
+  console.log(url)
+  await zip.file(file_name, urlToPromise(url), { binary: true })
+  
+ }
+
+function export_csv() {
+  //export csv files
+}
+
+
+function urlToPromise(url) {
+  return new Promise(function(resolve, reject) {
+    JSZipUtils.getBinaryContent(url, function (err, data) {
+          if(err) {
+              reject(err);
+          } else {
+              resolve(data);
+          }
       });
+  });
+  }
+
+
+
+
+  d3.select("#download")
+    .on('click', function () {
+
+     
+      export_plots();
+      // export_tables();
+      // export_csv();
+
+      
+
+    });
+
+
+      // image_array = [];
+      // var zip = new JSZip();
+      // prime_count = 0
+      // new_count = 0;
+      // $(".dl-files").children().each(function () {
+      //   tempChk = $(this).children()[0];
+      //   if (tempChk.checked) { 
+      //     prime_count += 1;
+      //   }
+      // })
+    //   return new Promise((resolve, reject) => {
+
+    //     
+    
+              // $("#hidden-graphs").append("img");
+              // var img = $('<img/>')
+              // img.attr("src", dataUrl)
+              // img.attr("class", "hidden-graph")
+              // img.appendTo("#hidden-graphs");
+              // src='" + dataUrl + "' class='hidden-graph'>
+              // img_png.attr("src", dataUrl);
+         
+
+              // new_image = saveAs(dataUrl, tempChk.value.slice(0, -7) +".png");
+        
+        
+          //     zip.file(tempChk.value.slice(0, -7) + ".png", urlToPromise(dataUrl), { binary: true });
+          //   })
+          
+          // });
+
+
+        
   
 
-  });
 
+    
+
+
+//       //reach here regardless
+//       // {status: "fulfilled", value: 33}
+//    });
+    
 
