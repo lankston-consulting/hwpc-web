2d13540ed9bba1ae90dcac3e86c7d775bff262e1
Claire Spain <clairespain@outlook.com>
2023-01-18 16:35:57 -0600

    Updated taboutput.js var to let

--- static/js/taboutput.js
+++ static/js/taboutput.js
@@ -1,7 +1,7 @@
 document.getElementById("defaultOpen").click();
 
 function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
